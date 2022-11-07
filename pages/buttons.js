70b28759aa49a918f7ce5102dd7d0f0ca62569e4
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2022-11-08 00:03:30 +0100

    sc

--- pages/buttons.js
+++ pages/buttons.js
@@ -91,7 +91,13 @@ export default function buttons() {
         </SyntaxHighlighter>   
 
       </span>
-    
+
+      <button class="rounded-full inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" >Save Changes</button>
+
+      <SyntaxHighlighter language="javascript">
+                 {'<button class="rounded-full inline-flex items-center/>'}
+        </SyntaxHighlighter>   
+
     </Layout>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,33 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
     <>
         <Layout>
-                <h1>Page Home..</h1>
+                <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+                    <div class="md:flex">
+                      <div class="md:shrink-0">
+                        <img class="h-48 w-full object-cover md:h-full md:w-48" 
+                            src="computer.png" alt="Modern building architecture"/>
+                      </div>
+                      <div class="p-8">
+                        <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Marlon Falcón Hernández</div>
+                          <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Sample tailwincss</a>
+                        <p class="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
+                      </div>
+                    </div>
+            </div>
+                <div class="flex flex-row justify-center items-center bg-purple-600 h-20 text-white">
+                    <Link href="https://www.svgrepo.com/">svgrepo icon</Link>
+                </div>
+
+                <div class="flex flex-row justify-center items-center bg-red-600 h-20 text-white">
+                    <Link href="https://undraw.co/illustrations">undraw svg</Link>
+                </div>
+
+                
+
         </Layout>
     </>
   )
